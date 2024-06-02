bcd483c4b1b2f2b47f7930346cd1281d21eed9a7
Anurag7645 <114188019+Anurag7645@users.noreply.github.com>
2024-06-02 14:10:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const { PINATA_API_KEY, PINATA_SECRET_KEY} = process.env;
 
 const pinataSDK = require('@pinata/sdk')
-const pinata = new pinataSDK('PINATA_SECRET_KEY','PINATA_API_KEY');
-
+const pinata = new pinataSDK(PINATA_API_KEY,PINATA_SECRET_KEY);
+console.log(PINATA_API_KEY,PINATA_SECRET_KEY);
 pinata.testAuthentication().then((result) => {
     console.log(result);
 }).catch((err) => {
